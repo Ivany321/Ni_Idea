080235dea99492173388d1895eb03dc87e1547da
Lenovo <Lenovo@DESKTOP-IO113LT>
2025-10-03 10:53:18 -0600

    Primercommit

--- src/main/java/tics/itess/Usuario.java
+++ src/main/java/tics/itess/Usuario.java
@@ -1,6 +1,11 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * ITESS-TICS TInvestigacion
+*  Agosto - Diciembre 2025
+*  Proyecto Niidea
+*  Clase Logro: Representa una habilidad adquirida mediante una evidencia
+*  Programador: Ivany Israel Rosas Torres
+*  Fecha: 02/10/2025
+ * 
  */
 package tics.itess;
 
@@ -11,14 +16,32 @@ package tics.itess;
 public class Usuario {
     
     private String nombre;
+    
+    /**
+     * Constructor de la clase Usuario.
+     *
+     * @param nombre Nombre del usuario
+     */
 
     public Usuario(String nombre) {
         this.nombre = nombre;
     }
+    
+    /**
+     * Obtiene el nombre del usuario.
+     *
+     * @return nombre del usuario
+     */
 
     public String getNombre() {
         return nombre;
     }
+    
+    /**
+     * Establece el nombre del usuario.
+     *
+     * @param nombre nuevo nombre del usuario
+     */
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
