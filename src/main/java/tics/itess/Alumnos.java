d882f6b6268a2716328b10cbfc76a7ff81f82d80
Lenovo <Lenovo@DESKTOP-IO113LT>
2025-10-03 10:44:27 -0600

    Primercommit

--- src/main/java/tics/itess/Alumnos.java
+++ src/main/java/tics/itess/Alumnos.java
@@ -1,6 +1,11 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * ITESS-TICS TInvestigacion
+*  Agosto - Diciembre 2025
+*  Proyecto Niidea
+*  Clase Logro: Representa una habilidad adquirida mediante una evidencia
+*  Programador: Ivany Israel Rosas Torres
+*  Fecha: 02/10/2025
+ * 
  */
 package tics.itess;
 
@@ -11,25 +16,54 @@ package tics.itess;
 public class Alumnos {
     private String numControl;
     private String nombre;
+    
+ /**
+ *
+ * Constructor de la clase alumnos
+ *  @param numControl Número de control del alumno
+ * @param nombre Nombre del alumno
+ */
 
     public Alumnos(String numControl, String nombre) {
         this.numControl = numControl;
         this.nombre = nombre;
     }
 
-    
+    /**
+     * Obtiene el número de control del alumno.
+     *
+     * @return número de control como cadena
+     */
 
     public String getNumControl() {
         return numControl;
     }
+    
+     /**
+     * Obtiene el nombre del alumno.
+     *
+     * @return nombre como cadena
+     */
 
     public String getNombre() {
         return nombre;
     }
+    
+     /**
+     * Establece el número de control del alumno.
+     *
+     * @param numControl nuevo número de control
+     */
 
     public void setNumControl(String numControl) {
         this.numControl = numControl;
     }
+    
+    /**
+     * Establece el nombre del alumno.
+     *
+     * @param nombre nuevo nombre del alumno
+     */
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
