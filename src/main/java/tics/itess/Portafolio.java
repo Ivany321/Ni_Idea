c6808bb912e6205fede3b21e9f4434e49656f46f
Lenovo <Lenovo@DESKTOP-IO113LT>
2025-10-03 10:51:51 -0600

    Primercommit

--- src/main/java/tics/itess/Portafolio.java
+++ src/main/java/tics/itess/Portafolio.java
@@ -1,6 +1,11 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * ITESS-TICS TInvestigacion
+*  Agosto - Diciembre 2025
+*  Proyecto Niidea
+*  Clase Logro: Representa una habilidad adquirida mediante una evidencia
+*  Programador: Ivany Israel Rosas Torres
+*  Fecha: 02/10/2025
+ * 
  */
 package tics.itess;
 
@@ -12,23 +17,54 @@ public class Portafolio {
     
     private String nombre;
     private String curso;
+    
+    /**
+     * Constructor de la clase Portafolio.
+     *
+     * @param nombre Nombre del portafolio
+     * @param curso Curso al que pertenece el portafolio
+     */
 
     public Portafolio(String nombre, String curso) {
         this.nombre = nombre;
         this.curso = curso;
     }
 
+    
+    /**
+     * Obtiene el nombre del portafolio.
+     *
+     * @return nombre del portafolio
+     */
     public String getNombre() {
         return nombre;
     }
+    
+    /**
+     * Establece el nombre del portafolio.
+     *
+     * @param nombre nuevo nombre del portafolio
+     */
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+    
+    /**
+     * Obtiene el curso al que pertenece el portafolio.
+     *
+     * @return curso del portafolio
+     */
 
     public String getCurso() {
         return curso;
     }
+    
+    /**
+     * Establece el curso al que pertenece el portafolio.
+     *
+     * @param curso nuevo curso del portafolio
+     */
 
     public void setCurso(String curso) {
         this.curso = curso;
