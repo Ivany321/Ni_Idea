65d22d100d2ace929e34e7b8432b8e0eca74cde4
Lenovo <Lenovo@DESKTOP-IO113LT>
2025-10-03 10:49:58 -0600

    Primercommit

--- src/main/java/tics/itess/Familiar.java
+++ src/main/java/tics/itess/Familiar.java
@@ -1,6 +1,11 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * ITESS-TICS TInvestigacion
+*  Agosto - Diciembre 2025
+*  Proyecto Niidea
+*  Clase Logro: Representa una habilidad adquirida mediante una evidencia
+*  Programador: Ivany Israel Rosas Torres
+*  Fecha: 02/10/2025
+ * 
  */
 package tics.itess;
 
@@ -12,23 +17,54 @@ public class Familiar {
     
     private String nombre;
     private String matriculaParentesco;
+    
+    /**
+     * Constructor de la clase Familiar.
+     *
+     * @param nombre Nombre del familiar
+     * @param matriculaParentesco Matrícula o parentesco asociado al familiar
+     */
 
     public Familiar(String nombre, String matriculaParentesco) {
         this.nombre = nombre;
         this.matriculaParentesco = matriculaParentesco;
     }
 
+    
+    /**
+     * Obtiene el nombre del familiar.
+     *
+     * @return nombre del familiar
+     */
     public String getNombre() {
         return nombre;
     }
+    
+     /**
+     * Establece el nombre del familiar.
+     *
+     * @param nombre nuevo nombre del familiar
+     */
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+    
+    /**
+     * Obtiene la matrícula o parentesco del familiar.
+     *
+     * @return matrícula o parentesco
+     */
 
     public String getMatriculaParentesco() {
         return matriculaParentesco;
     }
+    
+    /**
+     * Establece la matrícula o parentesco del familiar.
+     *
+     * @param matriculaParentesco nueva matrícula o parentesco
+     */
 
     public void setMatriculaParentesco(String matriculaParentesco) {
         this.matriculaParentesco = matriculaParentesco;
