d8c80263ddd03b8205fef238f8ce97a0bf6f7ac0
Lenovo <Lenovo@DESKTOP-IO113LT>
2025-10-03 10:45:01 -0600

    Primercommit

--- src/main/java/tics/itess/Logro.java
+++ src/main/java/tics/itess/Logro.java
@@ -18,33 +18,79 @@ public class Logro {
     private int id;
     private String categoria;
     private String descripcion;
+    
+     /**
+ *
+ * Constructor de la clase Logro
+ *  @param id ID del logro
+ * @param categoria Categoria del logro
+ * @param descripcion Descripcion del logro
+ */
 
     public Logro(int id, String categoria, String descripcion) {
         this.id = id;
         this.categoria = categoria;
         this.descripcion = descripcion;
     }
+    
+    /**
+     * Obtiene el id del logro.
+     *
+     * @return id
+     */
 
     public int getId() {
         return id;
     }
+    
+    
+    /**
+     * Establece el id del logro.
+     *
+     * @param id nuevo id del logro
+     */
 
     public void setId(int id) {
         this.id = id;
     }
+    
+    
+    /**
+     * Obtiene la categoría del logro.
+     *
+     * @return categoría del logro
+     */
 
     public String getCategoria() {
         return categoria;
     }
+    
+    /**
+     * Establece la categoría del logro.
+     *
+     * @param categoria nueva categoría del logro
+     */
 
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
+    
+    /**
+     * Obtiene la descripción del logro.
+     *
+     * @return descripción del logro
+     */
 
     public String getDescripcion() {
         return descripcion;
     }
 
+    
+    /**
+     * Establece la descripción del logro.
+     *
+     * @param descripcion nueva descripción del logro
+     */
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
